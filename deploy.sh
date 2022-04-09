4884435c3abb3df9d8c8301399a8c771cd7a9da3
aifighter <phillu@126.com>
2022-04-09 19:57:05 +0800

    update static files

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,6 @@
 python3 resize_images.py
 git add .
 git commit -m 'update static files'
-git remote set-url origin https://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@github.com/aifighter/aifighter.github.io.git
 git push origin hexo
 # nvm use 12
 hexo clean
